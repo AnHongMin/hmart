92acf92ceac77951290ab06430e992ed53a871fd
hongmin.an <hongmin.an@HQ-IT45.hmart.com>
2014-01-21 11:33:05 -0500

    test

--- BASE_GIT/web/vm/sample/test_chart.js
+++ BASE_GIT/web/vm/sample/test_chart.js
@@ -13,13 +13,12 @@ Ext.onReady(function() {
 			url : '/vm/sample/stateData.vm',
 			method : 'GET'
 		}, 			
-		fields : [ { name : 'stateid', mapping : 'stateid' }, 
-		           { name : 'statename', mapping : 'statename'}
+		fields : [ { name : 'regionid', mapping : 'regionid' }, 
+		           { name : 'regionname', mapping : 'regionname'}
 		],
-		autoLoad : true,
+		autoLoad : false,
 		listeners : {
  			load : function() {
- 				Ext.getCmp('hr-search-form-state-combo').select('1');
  			}
  		}
 	}); 
@@ -42,7 +41,7 @@ Ext.onReady(function() {
 		fields : [ { name : 'branchid', mapping : 'branchid' },
 		           { name : 'branchname', mapping : 'branchname'}
 		],
-		autoLoad : true,
+		autoLoad : false,
 		listeners : {
  			load : function() {
  	 			Ext.getCmp('hr-search-form-branch-combo').select(this.getAt(0).get('branchid'));
@@ -55,22 +54,21 @@ Ext.onReady(function() {
     	storeId : 'hr_chartStore',
     	proxy : {
     		type : 'ajax',
-    		url : '/sample.do?method=testData',
+    		url : '/sample.do?method=chartData',
     		reader : {
     			type : 'json',
-    			root : 'dataset',
-    			idProperty : 'operator'
+    			root : 'data'
 			},
 			// 확장 파라미터 설정
 			extraParams: {
-				searchState : "1",
-				searchDateFrom : "",
-				searchBrach : "1",
-				searchDateTo : "",
-				searchDateType : ""	             
+				stateid : "0",
+				branchid : "0",
+				datefrom : "0",
+				dateto : "0",
+				dates : "0"
 			}
     	},
-		fields : [ 'operator', 'transaction', 'ringtime', 'net', 'item'],
+		fields : [ 'BRANCHID', 'HOUR', 'WORKINGMIN'],
 		autoLoad : true
 	});
 
@@ -83,90 +81,109 @@ Ext.onReady(function() {
         collapsible: true,
         bodyPadding: 5,
         items : [{
-        	xtype : 'container',
-        	layout : 'column',
-        	items : [{
-        		xtype : 'container',
-        		columnWidth : .3,
-        		items : [{
-        			id : 'hr-search-form-state-combo',
-        			xtype: 'combobox',		
-        			fieldLabel: 'State',
-        			name: 'searchState',
-        			anchor:'95%',
-        			store:hr_states_store,
-        			displayField:'statename',
-        			valueField:'stateid',
+			xtype : 'container',
+			layout : 'column',
+			items : [
+			    {
+			    	xtype : 'displayfield',
+			    	columnWidth : .05
+			    },{
+				xtype : 'container',
+				columnWidth : .3,
+				items : [{
+					id : 'hr-search-form-state-combo',
+					xtype: 'combobox',		
+					fieldLabel: 'State',
+					name: 'searchState',
+					anchor:'95%',
+					store:hr_states_store,
+					displayField:'regionname',
+					valueField:'regionid',
 					editable : false,
 					allowBlank : false,
-        			listeners: {
-        				select: function() {
-        					hr_branch_store.getProxy().extraParams.stateid = this.getValue();
-        					hr_branch_store.load();
-        				}
-        			}
-        		},{
-        			xtype: 'datefield',
-        			fieldLabel: 'Date From',
-        			name: 'searchDateFrom',
-        			format : 'Y-m-d',
-        			anchor:'95%',
+					listeners: {
+						select: function() {
+							hr_branch_store.getProxy().extraParams.regionid = this.getValue();
+							hr_branch_store.load();
+						}
+					}
+				},{
+					xtype: 'datefield',
+                	fieldLabel: 'Date From',
+                	name: 'searchDateFrom',
+                	format : 'Y-m-d',
+                	anchor:'95%',
+                	editable : false,
+                	allowBlank : false,
+                	// TEST VALUE
+                	//value : '2013-01-01'                	        		  
+				}]
+			},{
+				xtype : 'container',
+				columnWidth : .3,
+				items : [{
+					id : 'hr-search-form-branch-combo',
+					xtype: 'combobox',
+					fieldLabel: 'Branch',
+					name: 'searchBrach',
+					anchor:'95%',
+					store:hr_branch_store,
+					displayField:'branchname',
+					valueField:'branchid',
 					editable : false,
 					allowBlank : false
-        		}]
-        	},{
-        		xtype : 'container',
-        		columnWidth : .3,
-        		items : [{
-        			id : 'hr-search-form-branch-combo',
-        			xtype: 'combobox',
-        			fieldLabel: 'Branch',
-        			name: 'searchBrach',
-        			anchor:'95%',
-        			store:hr_branch_store,
-        			displayField:'branchname',
-        			valueField:'branchid',
+				},{
+					xtype: 'datefield',
+					fieldLabel: 'Date To',
+					format : 'Y-m-d',
+					name: 'searchDateTo',
+					anchor:'95%',
 					editable : false,
-					allowBlank : false
-        		},{
-        			xtype: 'datefield',
-        			fieldLabel: 'Date To',
-        			name: 'searchDateTo',
-        			format : 'Y-m-d',
-        			anchor:'95%',
-					editable : false,
-					allowBlank : false
-        		}]
-        	},{
-        		layout : 'form',
-        		columnWidth : .3,
-        		items: [{
-        			xtype: 'button',
-        			text: 'SEARCH',
-        			width : 200,
-        			handler: function() {
-        				var form = this.up('form').getForm();
-        				if(form.isValid()){
-        					var values = form.getValues();
-        					hr_chartStore.getProxy().extraParams.searchState = values['searchState'];
-        					hr_chartStore.getProxy().extraParams.searchDateFrom = values['searchDateFrom'];
-        					hr_chartStore.getProxy().extraParams.searchBrach = values['searchBrach'];
-        					hr_chartStore.getProxy().extraParams.searchDateTo = values['searchDateTo'];
-        					hr_chartStore.getProxy().extraParams.searchDateType = values['searchDateType'];
-        					hr_chartStore.load();
-        				}
-        			}
-        		},{
-        			xtype: 'button',
-        			text: 'RESET',
-        			width : 200,
-        			handler: function() {
-        				this.up('form').getForm().reset();
-        				hr_branch_store.removeAll();
-        			}
-        		}]
-        	},{
-        		columnWidth : .6,
+					allowBlank : false,
+					// TEST VALUE
+					//value : '2013-01-10',                	        		  
+				}]
+			},{
+				layout : 'form',
+				columnWidth : .3,
+				items: [{
+					xtype: 'button',
+					text: 'SEARCH',
+					width : 200,
+					handler: function() {
+						var form = this.up('form').getForm();
+						var values = form.getValues(); 
+						if(form.isValid() && (values['searchDateFrom'] <= values['searchDateTo']) ){                	        				                	        				 
+							hr_chartStore.getProxy().extraParams.stateid = values['searchState'];
+             	        	hr_chartStore.getProxy().extraParams.branchid = values['searchBrach'];
+							hr_chartStore.getProxy().extraParams.datefrom = values['searchDateFrom'];
+							hr_chartStore.getProxy().extraParams.dateto = values['searchDateTo'];
+							hr_chartStore.getProxy().extraParams.dates = values['searchDateType'];
+							hr_chartStore.load();                	        				  
+						} else {
+							Ext.Msg.alert("Warning","Search condition is not correct. One or more field is not valid.");
+						}
+					}
+				},{
+					xtype: 'button',
+					text: 'RESET',
+					width : 200,
+					handler: function() {
+						this.up('form').getForm().reset();
+						hr_chartStore.removeAll();
+						Ext.getCmp('resultForm').getForm().reset();						
+					}
+				}]
+			}]
+        }, {
+			xtype : 'container',
+			layout : 'column',
+			items : [
+			    {
+		    	xtype : 'displayfield',
+		    	columnWidth : .05
+		        },{
+				columnWidth : .6,
 				xtype: 'checkboxgroup',
 				columns: 8,
 				allowBlank : false,
@@ -180,12 +197,23 @@ Ext.onReady(function() {
 					{ boxLabel: 'SAT', name: 'searchDateType', inputValue: '6' },
 					{ boxLabel: 'SUN', name: 'searchDateType', inputValue: '7' }
 				]
-        	}]
+			}]
         }]
     });
     
+    // 선택된 cashier 배경색 지정 
+    var hr_chart_color = function(index){
+    	 var value = index % 5;
+         var color = ['rgb(44, 153, 201)', 
+                      'rgb(213, 70, 121)', 
+                      'rgb(107, 102, 255)', 
+                      'rgb(49, 149, 0)', 
+                      'rgb(249, 153, 0)'][value];
+         return color;
+    };
+    
     // 차트 출력(그리드)
-    var hr_chart = Ext.create('Ext.chart.Chart', {
+    var hr_workingmin_chart = Ext.create('Ext.chart.Chart', {
     	animate: true,
     	style: 'background:#fff',
     	shadow: false,
@@ -195,16 +223,16 @@ Ext.onReady(function() {
     	axes: [{
     		type: 'Category',
     		position: 'bottom',
-    		fields: ['operator'],
-    		title: 'OPERATOR'
+    		fields: ['HOUR'],
+    		title: 'HOUR'
     	}, {
     		type: 'Numeric',
     		position: 'left',
-    		fields: ['transaction'],
+    		fields: ['WORKINGMIN'],
     		label: {
     			renderer: Ext.util.Format.numberRenderer('0,0')
     		},
-    		title: 'Rate(%)',
+    		title: 'WORKINGMIN',
     		minimum: 0
     	}],
     	series: [{
@@ -212,29 +240,62 @@ Ext.onReady(function() {
             axis: 'left',
             label: {
             	display: 'insideEnd',
-                field: 'transaction',
+                field: 'WORKINGMIN',
                 renderer: Ext.util.Format.numberRenderer('0'),
                 orientation: 'horizontal',
                 color: '#333',
                 'text-anchor': 'middle'
             },
-            xField: 'operator',
-            yField: ['transaction'],
+            xField: 'HOUR',
+            yField: ['WORKINGMIN'],
             renderer: function(sprite, record, attr, index, store) {
-            	//var fieldValue = Math.random() * 20 + 10;
-                var value = (record.get('transaction') >> 0) % 5;
-                var color = ['rgb(213, 70, 121)', 
-                             'rgb(44, 153, 201)', 
-                             'rgb(146, 6, 157)', 
-                             'rgb(49, 149, 0)', 
-                             'rgb(249, 153, 0)'][value];
-                return Ext.apply(attr, {
-                    fill: color
-                });
+            	 return Ext.apply(attr, {
+                     fill: hr_chart_color(index)
+                 });
             },
             listeners : {
             	itemmouseup : function(obj) {
-            		Ext.getCmp('searchCashier').setValue(obj.storeItem.data['operator']);
+            	}
+            }
+    	}]
+    });
+    
+    var EXPWORKINGMIN = 20;//Ext.getCmp('EXPWORKINGMIN').getValue();
+    var hr_workingman_chart = Ext.create('Ext.chart.Chart', {
+    	autoLoad : false,
+    	animate: true,
+    	style: 'background:#fff',
+    	shadow: false,
+    	store: hr_chartStore,
+    	width: 800,
+    	height: 300,
+    	axes: [{
+    		type: 'Category',
+    		position: 'bottom',
+    		fields: ['HOUR'],
+    		title: 'HOUR'
+    	}, {
+    		type: 'Numeric',
+    		position: 'left',
+    		fields: ['WORKINGMIN'],
+    		label: {
+    			renderer: Math.round(Ext.util.Format.numberRenderer('0,0') / EXPWORKINGMIN)
+    		},
+    		title: 'WORKINGMAN',
+    		minimum: 0
+    	}],
+    	series: [{
+            type: 'column',
+            axis: 'left',
+            xField: 'HOUR',
+            yField: ['WORKINGMIN'],
+            renderer: function(sprite, record, attr, index, store) {
+            	 return Ext.apply(attr, {
+                     fill: hr_chart_color(index)
+                 });
+            },
+            listeners : {
+            	itemmouseup : function(obj) {
             	}
             }
     	}]
@@ -249,7 +310,7 @@ Ext.onReady(function() {
     		layout : 'column',
     		items : [{
     			xtype : 'displayfield',
-    			columnWidth : .1           			   
+    			columnWidth : .30           			   
     		},{
     			xtype : 'container',
     			columnWidth : .9,
@@ -258,17 +319,12 @@ Ext.onReady(function() {
     				xtype : 'fieldcontainer',
     				layout : 'hbox',
     				items: [{
-    					xtype : 'displayfield',
-    					value : 'Best Performance Cashier ',
-    					width : 200
-    				},{
     					xtype : 'textfield',
-    					name : 'searchCashier',
+    					name : 'EXPWORKINGMIN',
     					width : 50,
     					allowBlank : false,
-    					id : 'searchCashier',
-    					readOnly :true,
-    					fieldStyle: 'background-color: #ddd; background-image: none;'
+    					id : 'EXPWORKINGMIN',
+    					value : 30
     				},{
     					xtype : 'displayfield',
     					width : 10
@@ -281,154 +337,16 @@ Ext.onReady(function() {
     						var values = form.getValues();
     						if (typeof console == "object") {
     							console.log(values);
+    							hr_workingman_chart.load();
     						}
     					}
-    				}]
-    			}, {
-    				xtype : 'fieldcontainer',
-    				layout : 'hbox',
-    				items: [{
-    					xtype : 'displayfield',
-    					value : 'Standard Rate',
-    					width : 200
-    				}, {
-    					xtype : 'numberfield',
-    					name: 'searchRate',
-    					id : 'hr-search-rate',
-    					width : 50,
-    					allowBlank : false,
-    					value : '85'
-    				}, {
-    					xtype : 'displayfield',
-    					width : 10							    
-    				}, {
-    					xtype : 'displayfield',
-    					value : '% Expected Sales Amount $',	
-    					width : 175
-    				}, {
-    					xtype : 'textfield',		
-    					name: 'searchAmount',
-    					width : 55,
-    					value : "10000" 
-    				},{
-    					xtype : 'displayfield',
-    					width : 10
-    				},{
-    					xtype : 'button',
-    					text : 'Confirm',
-    					width : 70,
-    					handler: function() {
-    						for(var i=1; i<6; i++){
-    							Ext.getCmp('result_data_'+i).setValue(this.up('form').getForm().getValues()['searchAmount']*i);	
-    						}
-    					}
-    				}]
-    			}, {
-    				xtype : 'fieldcontainer',
-    				layout : 'hbox',
-    				items : [{
-    					xtype : 'displayfield',
-    					value : 'Needed',
-    					width : 55								
-    				}, {
-    					xtype : 'textfield',
-    					id : 'result_data_1',
-    					width : 50,
-    					readOnly : true,
-    					fieldStyle: 'background-color: #ddd; background-image: none;'
-    				}, {
-    					xtype : 'displayfield',
-    					width : 10							    
-    				}, {
-    					xtype : 'displayfield',
-    					value: 'Standard cashiers on selected date'							    
-    				}]
-    			}, {
-    				xtype : 'fieldcontainer',
-    				layout : 'hbox',
-    				items : [{
-    					xtype : 'displayfield',
-    					value : 'Total',
-    					width : 55								
-    				}, {
-    					xtype : 'textfield',
-    					id : 'result_data_2',
-    					width : 50,
-    					readOnly : true,
-    					fieldStyle: 'background-color: #ddd; background-image: none;' 
-    				}, {      
-    					xtype : 'displayfield',
-    					width : 10							    
-    				}, {      
-    					xtype : 'displayfield',
-    					value : 'man-hour expected'							    
-    				}]
-    			}, {
-    				xtype : 'fieldcontainer',
-    				layout : 'hbox',
-    				items : [{
-    					xtype : 'displayfield',
-    					value : 'They Can cover',
-    					width : 100								
-    				}, {
-    					xtype : 'textfield',
-    					id : 'result_data_3',
-    					width : 50,
-    					readOnly : true,
-    					fieldStyle: 'background-color: #ddd; background-image: none;'
-    				}, {      
-    					xtype : 'displayfield',
-    					width : 10							    
-    				}, {      
-    					xtype : 'displayfield',
-    					value : 'transactions'							    
-    				}]
-    			}, {
-    				xtype : 'fieldcontainer',
-    				layout : 'hbox',
-    				items : [{
-    					xtype : 'displayfield',		
-    					value : 'They Can cover',				
-    					width : 100
-    				}, {
-    					xtype : 'textfield',
-    					id : 'result_data_4',
-    					width : 50,
-    					readOnly : true,
-    					fieldStyle: 'background-color: #ddd; background-image: none;'
-    				}, {      
-    					xtype : 'displayfield',		
-    					width : 10
-    				}, {
-    					xtype : 'displayfield',
-    					value : 'items'
-    				}]
-    			}, {
-    				xtype : 'fieldcontainer',
-    				layout : 'hbox',
-    				items : [{
-    					xtype : 'displayfield',		
-    					value : 'They Can cover',				
-    					width : 100
-    				}, {
-    					xtype : 'textfield',
-    					id : 'result_data_5',
-    					width : 50,
-    					readOnly : true,
-    					fieldStyle: 'background-color: #ddd; background-image: none;'
-    				}, {
-    					xtype : 'displayfield',		
-    					width : 10							    
-    				}, {
-    					xtype : 'displayfield',		
-    					value : 'amount sales'							    
     				}]
     			}]
     		}]
     	}]
     });
             	  
-    var hr_chartTab = {
+    var hr_workingmin_chartTab = {
        	xtype: 'container',
        	layout : 'column',
        	items : [{
@@ -437,25 +355,27 @@ Ext.onReady(function() {
 		},{
 			xtype : 'container',
 			columnWidth : .9,
-			items : [ hr_chart ]
+			items : [ hr_workingmin_chart ]
        	}]
 	};
     
-    var hr_descTab = {
-    	xtype: 'fieldset',
-    	title: 'Description',
-    	collapsible: true,
-    	collapsed : true,
-    	items: [{
-    		xtype : 'form',
-    		html : 'abcdefg<br/>  abcdefg<br/>      abcdefg'
-    	}]
-    };
+    var hr_workingman_chartTab = {
+       	xtype: 'container',
+       	layout : 'column',
+       	items : [{
+			xtype : 'displayfield',
+			columnWidth : .05           			   
+		},{
+			xtype : 'container',
+			columnWidth : .9,
+			items : [ hr_workingman_chart ]
+       	}]
+	};
     
     var hr_panel = Ext.create('Ext.panel.Panel', {
     	bodyPadding: 5,
     	width : 1000,
-    	items: [hr_searchTab, hr_chartTab, hr_descTab, hr_resultTab],
+    	items: [hr_searchTab, hr_workingmin_chartTab, hr_resultTab, hr_workingman_chartTab],
     	renderTo: Ext.getBody()
     });
 });
